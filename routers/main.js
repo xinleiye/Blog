20728015d7783b557bc0615391f8757882888f6c
xinleiye <xinlei_ye@163.com>
2017-10-12 16:10:55 +0800

    完成首页导航功能
    1. 从数据库中读取分类，显示在首页导航菜单中

--- routers/main.js
+++ routers/main.js
@@ -1,12 +1,20 @@
 
 var express = require("express");
+var category = require("../models/category");
+
 var homeRouter = express.Router();
 
 homeRouter.get("/", function (req, res, next) {
 
-    res.render("main/index", {
-        userInfo: req.userInfo
+    category.find().then(function(data) {
+        res.render("main/index", {
+            userInfo: req.userInfo,
+            categories: data
+        });
     });
+    /*res.render("main/index", {
+        userInfo: req.userInfo
+    });*/
 })
 
 module.exports = homeRouter;
