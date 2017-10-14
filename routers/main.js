28a9b42e6aede85a93bd9090e18aa387dcc71f12
xinleiye <xinlei_ye@163.com>
2017-10-14 11:57:45 +0800

    实现首页内容分类导航操作逻辑
    1. 单击分类时选中，同时取消其他分类的选中状态。

--- routers/main.js
+++ routers/main.js
@@ -6,15 +6,18 @@ var homeRouter = express.Router();
 
 homeRouter.get("/", function (req, res, next) {
 
+    var cate;
+
+    cate = req.query.category;
+
     category.find().then(function(data) {
         res.render("main/index", {
             userInfo: req.userInfo,
-            categories: data
+            categories: data,
+            category: cate
         });
     });
-    /*res.render("main/index", {
-        userInfo: req.userInfo
-    });*/
+
 })
 
 module.exports = homeRouter;
