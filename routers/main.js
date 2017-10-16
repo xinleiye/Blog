040aa7a1490eb8c15dbceb03de2a1bdb177573e0
xinleiye <xinlei_ye@163.com>
2017-10-16 11:46:36 +0800

    获取内容数据
    1. 从数据库中获取到分类数据，并保存到data中。
    2. 从数据库中获取内容数据，计算出页数，并按时间从新到旧排序，最后保存到data中
    2. 将data传递给 main/index 页面

--- routers/main.js
+++ routers/main.js
@@ -1,23 +1,50 @@
 
 var express = require("express");
 var category = require("../models/category");
+var content = require("../models/content");
 
 var homeRouter = express.Router();
 
-homeRouter.get("/", function (req, res, next) {
+//用于存储传递给 main/index 页面的数据
+var data = {};
 
-    var cate;
+homeRouter.use(function (req, res, next) {
+    data.userInfo = req.userInfo;
 
-    cate = req.query.category;
-
-    category.find().then(function (data) {
-        res.render("main/index", {
-            userInfo: req.userInfo,
-            categories: data,
-            category: cate
-        });
+    category.find().then(function (categories) {
+        data.categories = categories;
+        next();
     });
 
-})
+});
+
+homeRouter.get("/", function (req, res, next) {
+
+    var page, pages, limit, skip;
+
+    data.category = req.query.category || "";
+
+    limit = 10;
+    page = Number(req.query.page) || 1;
+
+    content.count().then(function (count) {
+        pages = Math.ceil(count / limit);
+        page = Math.min(page, pages);
+        page = Math.max(page, 1);
+
+        data.count = count;
+        data.page = page;
+        data.pages = pages;
+        data.limit = limit;
+        skip = (page - 1) * limit;
+
+        return content.find().limit(limit).skip(skip).populate(["category", "user"]).sort({addTime: -1});
+
+    }).then(function (contents) {
+        data.contents = contents;
+        res.render("main/index", data);
+    });
+
+});
 
 module.exports = homeRouter;
